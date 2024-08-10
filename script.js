c03d14dd92f6a2eb11c10d2c4a73ac4643b5ff99
Alex <149239010+DeusEx01@users.noreply.github.com>
2024-08-10 09:51:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 let toggle = document.querySelector(".toggle");
 let menu = document.querySelector(".menu");
-
+let button = document.querySelector(".radial-button");
 toggle.onclick = function (event) {
-  menu.classList.toggle("active");
+  console.log("Main button was clicked");
 };
+
+menu.addEventListener("click", (event) => {
+  if (!event.target.closest(".radial-button")) return;
+  let button = event.target.closest(".radial-button");
+  console.log(button.firstElementChild.textContent);
+  event.preventDefault();
+});
+
+menu.style.display = "none";
+document.addEventListener("keydown", (event) => {
+  if (event.code == "KeyB") {
+    menu.style.display = "";
+  }
+  event.preventDefault();
+});
+
+document.addEventListener("keyup", (event) => {
+  if (event.code == "KeyB") {
+    menu.style.display = "none";
+  }
+
+  event.preventDefault();
+});
