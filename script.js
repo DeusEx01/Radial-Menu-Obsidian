c48f162bd1be64f35631d75513ab9b4a8c14a409
Alex <149239010+DeusEx01@users.noreply.github.com>
2024-08-12 12:46:48 +0300

    Added basic functionality
    1. Added submenu
    2. Added keyboard support

--- script.js
+++ script.js
@@ -1,28 +1,95 @@
-let toggle = document.querySelector(".toggle");
-let menu = document.querySelector(".menu");
-let button = document.querySelector(".radial-button");
-toggle.onclick = function (event) {
-  console.log("Main button was clicked");
-};
-
-menu.addEventListener("click", (event) => {
-  if (!event.target.closest(".radial-button")) return;
-  let button = event.target.closest(".radial-button");
-  console.log(button.firstElementChild.textContent);
-  event.preventDefault();
-});
-
-menu.style.display = "none";
+const radial_menu = document.querySelector("#radial_menu_main");
 document.addEventListener("keydown", (event) => {
-  if (event.code == "KeyB") {
-    menu.style.display = "";
+  if (event.code == "F6") {
+    if (event.repeat) {
+      return;
+    }
+    radial_menu.classList.remove("radial_menu_hidden");
+    console.log(radial_menu.classList);
   }
-  event.preventDefault();
 });
 
 document.addEventListener("keyup", (event) => {
-  if (event.code == "KeyB") {
-    menu.style.display = "none";
+  if (event.code == "F6") {
+    radial_menu.classList.add("radial_menu_hidden");
+    let submenu = document.querySelector("#radial_submenu-6");
+    submenu.classList.add("radial_menu_hidden");
+  }
+});
+radial_menu.addEventListener("click", (event) => {
+  if (!event.target.closest("[data-id]")) return;
+  let button = event.target.closest("[data-id]");
+  switch (button.dataset.id) {
+    case "submenu-6":
+      console.log(button.dataset.id);
+      break;
+    case "1":
+      console.log(button.dataset.id);
+      break;
+    case "2":
+      console.log(button.dataset.id);
+      break;
+    case "3":
+      console.log(button.dataset.id);
+      break;
+    case "4":
+      console.log(button.dataset.id);
+      break;
+    case "5":
+      console.log(button.dataset.id);
+      break;
+    case "6":
+      console.log(button.dataset.id);
+      radial_menu.classList.add("radial_menu_hidden");
+      let submenu = document.querySelector("#radial_submenu-6");
+      submenu.classList.toggle("radial_menu_hidden");
+      console.log(submenu);
+      break;
+    case "7":
+      console.log(button.dataset.id);
+      break;
+    case "8":
+      console.log(button.dataset.id);
+      break;
+  }
+
+  event.preventDefault();
+});
+
+let submenu = document.querySelector("#radial_submenu-6");
+submenu.addEventListener("click", (event) => {
+  if (!event.target.closest("[data-id]")) return;
+  let button = event.target.closest("[data-id]");
+  switch (button.dataset.id) {
+    case "submenu-6":
+      radial_menu.classList.remove("radial_menu_hidden");
+      let submenu = document.querySelector("#radial_submenu-6");
+      submenu.classList.add("radial_menu_hidden");
+      break;
+    case "1":
+      console.log(button.dataset.id);
+      break;
+    case "2":
+      console.log(button.dataset.id);
+      break;
+    case "3":
+      console.log(button.dataset.id);
+      break;
+    case "4":
+      console.log(button.dataset.id);
+      break;
+    case "5":
+      console.log(button.dataset.id);
+      break;
+    case "6":
+      console.log(button.dataset.id);
+      break;
+    case "7":
+      console.log(button.dataset.id);
+      break;
+    case "8":
+      console.log(button.dataset.id);
+      break;
   }
 
   event.preventDefault();
